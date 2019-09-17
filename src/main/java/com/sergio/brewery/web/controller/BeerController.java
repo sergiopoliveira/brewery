91164e540973b626d993797a15980c1152a771db
sergiopoliveira <sergiopoliveira@outlook.com>
2019-09-17 19:58:45 +0100

    put mapping

--- src/main/java/com/sergio/brewery/web/controller/BeerController.java
+++ src/main/java/com/sergio/brewery/web/controller/BeerController.java
@@ -33,4 +33,10 @@ public class BeerController {
 
         return new ResponseEntity(headers, HttpStatus.CREATED);
     }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, BeerDto beerDto) {
+        beerService.updateBeer(beerId, beerDto);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
 }
