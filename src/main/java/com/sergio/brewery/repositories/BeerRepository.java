f7eae306800e6d57bcd197793a1a6ae786958b96
sergiopoliveira <sergiopoliveira@outlook.com>
2019-09-28 14:11:00 +0100

    beer repository

---
+++ src/main/java/com/sergio/brewery/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.sergio.brewery.repositories;
+
+import com.sergio.brewery.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
